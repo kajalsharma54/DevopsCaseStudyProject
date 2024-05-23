7a0dfe739285740b3d8281a254fc5dd710572dba
Kajal Sharma <ruhisharma9807@gmail.com>
2024-05-23 17:36:30 +0530

    Added 2 new project and files

---
+++ DevopsCaseStudyProject/BankAccount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DevopsCaseStudyDemoProject
+{
+    public class BankAccount
+    {
+        string _customerName;
+        double _balance;
+
+        public BankAccount(string customerName, double amount)
+        {
+            _customerName = customerName;
+            _balance = amount;
+        }
+
+        public double Balance { get { return _balance; } }
+
+        public void Debit(double amount)
+        {
+            if (_balance == 0)
+                throw new Exception("Balance is 0");
+            if (amount <= 0 || amount > _balance)
+                throw new ArgumentOutOfRangeException("Amount = 0 or Amount > Balance");
+            _balance -= amount;
+        }
+
+        public void Credit(double amount)
+        {
+            if (amount <= 0)
+                throw new ArgumentOutOfRangeException("Amount <= 0");
+            _balance += amount;
+        }
+    }
+}
---
+++ DevopsCaseStudyProjectTest/BankAccountTest.cs
@@ -0,0 +1,53 @@
+﻿using DevopsCaseStudyDemoProject;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DevopsCaseStudyDemoProjectTest
+{
+    [TestClass]
+    public class BankAccountTest
+    {
+        [TestMethod]
+        public void ExceptionCaseTest()
+        {
+            BankAccount bank = new BankAccount("Lucky", 0);
+
+            try
+            {
+                bank.Debit(2);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                StringAssert.Contains(e.Message, "Amount <= 0 or Amount > Balance");
+            }
+            catch (Exception e)
+            {
+                StringAssert.Contains(e.Message, "Balance is 0");
+            }
+        }
+
+        [TestMethod]
+        public void DebitTest()
+        {
+            BankAccount bank = new BankAccount("Lucky", 2);
+            bank.Debit(2);
+            Assert.AreEqual(0, bank.Balance);
+        }
+
+        [TestMethod]
+        public void CreditTest()
+        {
+            BankAccount bank = new BankAccount("Lucky", 2);
+            bank.Credit(2);
+            Assert.AreEqual(4, bank.Balance);
+        }
+
+        [TestMethod]
+        public void CreditDebitTest()
+        {
+            BankAccount bank = new BankAccount("Lucky", 2);
+            bank.Debit(2);
+            bank.Credit(2);
+            Assert.AreEqual(2, bank.Balance);
+        }
+    }
+}
